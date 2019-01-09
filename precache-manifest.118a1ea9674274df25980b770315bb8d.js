18df674a07895d2d2d9f980ceef765c9f33f0eaa
sailesh97 <33670045+sailesh97@users.noreply.github.com>
2019-01-09 21:08:07 +0530

    Updates

---
+++ precache-manifest.118a1ea9674274df25980b770315bb8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilding/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "e9ad1f8d898e0af83400",
+    "url": "/BurgerBuilding/static/js/runtime~main.e9ad1f8d.js"
+  },
+  {
+    "revision": "1b3733ce9c8d6a57dffb",
+    "url": "/BurgerBuilding/static/js/main.1b3733ce.chunk.js"
+  },
+  {
+    "revision": "43083dc4d84457486e31",
+    "url": "/BurgerBuilding/static/js/1.43083dc4.chunk.js"
+  },
+  {
+    "revision": "1b3733ce9c8d6a57dffb",
+    "url": "/BurgerBuilding/static/css/main.a7b3fe64.chunk.css"
+  },
+  {
+    "revision": "b819004f14d5779d180bae2a4b8345da",
+    "url": "/BurgerBuilding/index.html"
+  }
+];
