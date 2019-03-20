cc440eb53df92ed8b9efa225ca9658dfc7e0d237
sailesh97 <saileshpatra10@gmail.com>
2019-03-20 10:21:13 +0530

    Before Authentication

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -20,7 +20,7 @@ export const purchaseBurgerStart = () => {
         type: actionTypes.PURCHASE_BURGER_START
     };
 }
-console.log('a');
+
 export const purchaseBurger = (orderData) => {
     return dispatch => {
         dispatch(purchaseBurgerStart());
@@ -30,7 +30,55 @@ export const purchaseBurger = (orderData) => {
             dispatch(purchaseBurgerSuccess(response.data.name,orderData));
         })
         .catch(error => {
+            console.log(error);
             dispatch(purchaseBurgerFail(error));
         });
     }
 }
+
+export const purchaseInit = () => {
+    return {
+        type: actionTypes.PURCHASE_INIT
+    };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    }
+};
+
+export const fetchOrdersFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    }
+};
+
+export const fetchOrdersStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START ,
+    }
+};
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('/orders.json')
+        .then(res => {
+            const fetchedOrders = [];
+            for(let key in res.data) {
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            dispatch(fetchOrdersSuccess(fetchedOrders));
+        })
+        .catch(err => {
+            dispatch(fetchOrdersFail(err));
+        }); 
+    }
+};
+
+
